91a02cb5992ce88a4c948bcbfb0399ce51166711
abderraouf2che <35004387+abderraouf2che@users.noreply.github.com>
2018-12-27 21:37:37 +0100

    Update negationClass.py

--- negationClass.py
+++ negationClass.py
@@ -35,5 +35,83 @@ print(invert_simple(sent))
 
 
 
+from nltk.stem.wordnet import WordNetLemmatizer
+from thesaurus import Word
+from nltk import word_tokenize, pos_tag
+
+sentence='He told us a very exciting adventure story.'
+
+def sent_negation(sentence):
+    
+    lem=WordNetLemmatizer()
+    tags3=pos_tag(word_tokenize(sentence))
+
+    new_st=[]
+    for i,word in enumerate(tags3):
+
+        if word[1]=='VBP':
+            if word[0] in ['am','are','have']:
+                if tags3[i+1][1]=='JJ':
+                    new_st.append(word[0])
+                    pass
+                else:
+                    new_st.append(word[0])
+                    new_st.append('not')
+
+            else:
+                w=Word(word[0])
+                if len(w.antonyms())>0:
+                    new_st.append(w.antonyms()[0])
+                else:
+                    new_st.append('do not')
+                    new_st.append(word[0])
+
+        elif word[1]=='JJ':
+            w=Word(word[0])
+            if len(w.antonyms())>0:
+                new_st.append(w.antonyms()[0])
+            else:
+                new_st.append('not')
+                new_st.append(word[0])
+
+        elif word[1]=='VBZ':
+            if word[0] in ["'s",'is','has']:
+                if tags3[i+1][1]=='JJ':
+                    new_st.append(word[0])
+                    pass
+                else:
+                    new_st.append(word[0])
+                    new_st.append('not')
+
+            else:
+                new_st.append('does not')
+                new_st.append(lem.lemmatize(word[0],"v"))
+        elif word[1]=='VBD':
+            if word[0] in ['was','were']:
+                if tags3[i+1][1]=='JJ':
+                    new_st.append(word[0])
+                    pass
+                else:
+                    new_st.append(word[0])
+                    new_st.append('not')
+            else:
+                new_st.append('did not')
+                new_st.append(lem.lemmatize(word[0],"v"))
+        elif word[1]=='MD':
+            if tags3[i+1][0] in ['not','n\'t']:
+                print(tags3[i+1][0])
+                new_st.append(word[0])
+
+            else:
+                new_st.append(word[0])
+                new_st.append('not')
+        elif word[0] in ['not','n\'t']:
+            pass
+        else:
+            new_st.append(tags3[i][0])
+    return " ".join(new_st)
+
+
+
 
 
