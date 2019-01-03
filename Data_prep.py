ec779201550e65bf2e0c45b4274d80e860f285f6
abderraouf2che <35004387+abderraouf2che@users.noreply.github.com>
2019-01-03 22:18:21 +0100

    Update Data_prep.py

--- Data_prep.py
+++ Data_prep.py
@@ -4,20 +4,9 @@ import re
 comments = pd.read_csv('~/Downloads/attack_annotated_comments.tsv', sep = '\t', index_col = 0)
 annotations = pd.read_csv('~/Downloads/attack_annotations.tsv',  sep = '\t')
 
-
-def data_prep(data_frame):
-    data=data_frame
-    new_set=[]
-    # len(data.values)
-    for i in range(0,len(data.values)):
-        q=data.values[i][0].replace("NEWLINE_TOKEN",'')
-        e=q.replace("``",'')
-        s=e.split('.')
-        new_set.append(s)
-    return new_set
     
-def clean_text(data_frame):
-    
+def clean_text(data):
+    data_frame=data
     data_frame=data_frame['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
     data_frame=data_frame.apply(lambda x: x.replace("TAB_TOKEN", " "))
     data_frame=data_frame.apply(lambda x: x.lower())
