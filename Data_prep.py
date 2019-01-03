eed7629862d2941baad12eb06e8644e13e3fa8f9
abderraouf2che <35004387+abderraouf2che@users.noreply.github.com>
2019-01-03 23:18:28 +0100

    Update Data_prep.py

--- Data_prep.py
+++ Data_prep.py
@@ -12,7 +12,7 @@ def clean_text(data):
     data_frame['comment']=data_frame['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
     data_frame['comment']=data_frame['comment'].apply(lambda x: x.lower())
     data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('[/(){}\[\]\|@,;]','',x)))
-    data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('[^0-9a-z #+_]',' ',x)))
+#     data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('[^0-9a-z #+_]',' ',x)))
     data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('`',' ',x)))
     data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub(' +',' ',x)))
     return data_frame
