d419084b6cacfae41d566b2358b77ed1aa37b328
abderraouf2che <35004387+abderraouf2che@users.noreply.github.com>
2019-01-03 17:06:51 +0100

    Update testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -1,7 +1,7 @@
 import urllib
 from inscriptis import get_text
 
-
+#This function tests the fitted model and detects if the sentences from the webpage are of the class or not,it returns them 
 def test_bin(fitted_model,link_web):
   url = link_web
   html = urllib.request.urlopen(url).read().decode('utf-8')
@@ -18,8 +18,11 @@ def test_bin(fitted_model,link_web):
       x=model.predict(x)
       if x==True:
         all_sen=all_sen+sen+' 1'
-  print('detected sentences :'+ all_sen)
+  return all_sen
 
+  
+#This function tests the fitted multi model and detects if the sentences 
+#from the webpage belong to one of the classes or not,it returns them 
   
 def testing_multi(fitted_model, link_web):
   url =link_web
@@ -41,5 +44,5 @@ def testing_multi(fitted_model, link_web):
         all_sen=all_sen+sen+' 2'
       elif x==3:
         all_sen=all_sen+sen+' 3'
-  print('detected sentences :'+ all_sen)
+  return all_sen
   
