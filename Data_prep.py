81061b41e4ef3c98c16f7a468821c6ef97fa4502
abderraouf2che <35004387+abderraouf2che@users.noreply.github.com>
2019-01-04 08:19:18 +0100

    Update Data_prep.py

--- Data_prep.py
+++ Data_prep.py
@@ -6,13 +6,19 @@ annotations = pd.read_csv('~/Downloads/attack_annotations.tsv',  sep = '\t')
 
     
 def clean_text(data):
-    data_frame=data
-    
-    data_frame['comment']=data_frame['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
-    data_frame['comment']=data_frame['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
-    data_frame['comment']=data_frame['comment'].apply(lambda x: x.lower())
-    data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('[/(){}\[\]\|@,;]','',x)))
-#     data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('[^0-9a-z #+_]',' ',x)))
-    data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('`',' ',x)))
-    data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub(' +',' ',x)))
-    return data_frame
+	data_frame=data
+
+	data_frame['comment']=data_frame['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
+	data_frame['comment']=data_frame['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
+	data_frame['comment']=data_frame['comment'].apply(lambda x: x.lower())
+	data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub(r'\d+', ' ',x)))
+	data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('[/(){}\[\]\|@,;]',' ',x)))
+    # data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('[^0-9a-z #+_]',' ',x)))
+
+	data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('`',' ',x)))
+	data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('=',' ',x)))
+	data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub('_',' ',x)))
+	data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub(':',' ',x)))
+
+	data_frame['comment']=data_frame['comment'].apply((lambda x: re.sub(' +',' ',x)))
+	return data_frame
