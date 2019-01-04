dc68609db024e928c7ada77ea5c252fb7903f2d6
abderraouf2che <35004387+abderraouf2che@users.noreply.github.com>
2019-01-04 10:51:28 +0100

    Update randomArticle.py

--- randomArticle.py
+++ randomArticle.py
@@ -2,19 +2,23 @@
 from inscriptis import get_text
 import urllib.request
 
-def random_sent(n_sentences=100):
-	n=n_sentences
-	random_sents=[]
-	for i in range(n):
+def random_sent(n_sentences=2):
+    n=n_sentences
+    random_sents=[]
+    for i in range(n):
+        url = "http://en.wikipedia.org/wiki/Special:Random"
+        html = urllib.request.urlopen(url).read().decode('utf-8')
+        text = get_text(html)
 
-		url = "http://en.wikipedia.org/wiki/Special:Random"
-		html = urllib.request.urlopen(url).read().decode('utf-8')
-
-		text = get_text(html)
-		text3=text.split('.')
-		text3=text3[1]
-		random_sents.append(text3)
-	return random_sents
+        text=text.split('\n')
+    # index=text.index('Jump to navigation Jump to search')
+    # print(index) 
+        for i,line in enumerate(text):
+            if ' is ' in line:
+                print(text[i])
+                random_sents.append(text[i])
+                break
+    return random_sents
 
 
 text3=random_sent()
